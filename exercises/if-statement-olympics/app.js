f95c50ce0e859986ab83b23de0d4d73adbb4cc37
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2019-11-14 18:26:33 -0700

    day 3 activities

---
+++ exercises/if-statement-olympics/app.js
@@ -0,0 +1,63 @@
+//preliminaries
+
+if (5 > 3) {
+	console.log("5 is greater than 3");
+}
+var animal = "cat"
+
+if (animal.length === 3) {
+	console.log("cat length is 3");
+}
+
+if ("cat" === "dog") {
+	console.log("same");
+} else {
+	console.log("not the same");
+}
+
+//Bronze Medal
+
+var person = {
+	name: "Bobby",
+	age: 12
+};
+
+if (person.age >= 18) {
+	console.log(person.name + " is allowed to go to the movie");
+} else {
+	console.log(person.name + " is not allowed to go to the movie");
+}
+
+if (person.name.startsWith("B")) {
+	console.log(person.name + " is allowed to go to the movie");
+} else {
+	console.log(person.name + " is not allowed to go to the movie");
+}
+
+if (person.name.startsWith("B") && person.age > 18) {
+	console.log(person.name + " is allowed to go to the movie");
+} else {
+	console.log(person.name + " is not allowed to go to the movie");
+}
+
+//Silver Medal
+
+if (1 === "1") {
+	console.log("strict");
+} else if (1 == "1") {
+	console.log("loose");
+} else {
+	console.log("not equal to all");
+}
+
+if (1 <= 2 && 2 === 4) {
+    console.log("yes")
+}
+
+
+//Gold Medal 
+
+if ("dog" == String) {
+    console.log("yes")
+} 
+
