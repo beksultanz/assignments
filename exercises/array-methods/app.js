f714f3464cf6724ef916330f70e336ee5e7d0126
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2019-11-18 20:10:52 -0700

    day 5 activities

---
+++ exercises/array-methods/app.js
@@ -0,0 +1,65 @@
+let fruit = ["banana", "apple", "orange", "watermelon"];
+let vegetables = ["carrot", "tomato", "pepper", "lettuce"];
+
+// console.log(fruit);
+
+// #1
+
+vegetables.pop();
+
+// console.log(vegetables);
+
+// #2 
+
+fruit.shift(); 
+
+// console.log(fruit); 
+
+
+// #3 
+
+let index = fruit.indexOf("orange")
+
+fruit.indexOf(index) 
+
+// console.log(index)
+
+// #4 
+
+fruit.push(1)
+
+// console.log(fruit) 
+
+// # 5 
+
+// console.log(vegetables.length)  
+
+// #6 
+
+vegetables.push(3)
+
+// console.log(vegetables)
+
+//#7 
+
+let food = fruit.concat(vegetables)
+
+// console.log(food)
+
+// #8 
+
+food.splice(4,2) 
+
+// console.log(food)
+
+// #9 
+
+food.reverse() 
+
+// console.log(food)
+
+// #10 
+
+food.join(" ")
+
+console.log(food)
