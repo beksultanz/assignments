4afb94a932eb894f885fac1c34c2280e7862e91a
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2019-11-21 00:07:03 -0700

    day 7 activities

--- exercises/dom-olympics-start/index.js
+++ exercises/dom-olympics-start/index.js
@@ -1,10 +1,16 @@
-
-
+// <h1></h1>
 let h1 = document.createElement("h1");
+let h2 = document.createElement("h2");
+
+// <h1>JavaScript Made This!!</h1>
 h1.textContent = "JavaScript Made This!!";
+
+// <h1 class="header">JavaScript Made This!!</h1>
+h1.className = "header";
+
+h2.className = "sub-title";
+h2.innerHTML = "<span>Sultan</span> wrote the JavsScript";
 
 document.getElementById("header").appendChild(h1);
 
-h1.className = "header"; 
-
-subTitle.innerHTML = "<span class="">Sultan</span> wrote the JavsScript"; 
+document.getElementById("header").appendChild(h2);
---
+++ exercises/simple-html-calculator/app.js
@@ -0,0 +1,55 @@
+let sum = document.getElementById("addition");
+console.log(addForm);
+
+addForm.addEventListener("submit", function(e) {
+	// It prevents browser from refreshing
+	e.preventDefault();
+
+	// It gets input values
+	// - addForm selects the whole form
+	// addForm.addNum1 select the whole input element
+	// addForm.addNum1.value, finally gets the value from selected input element.
+	let input1 = parseInt(addForm.addNum1.value);
+	let input2 = parseInt(addForm.addNum2.value);
+
+	// It creates new DIV element. But, it does not render(show) in DOM(browser)
+	let sumResult = document.createElement("div");
+
+	// It add class result to newly created DIV element
+	sumResult.className = "result";
+
+	// Untill now, we have DIV with class name Result. But our DIV is empty. So, we need to insert value into it. For use value is the result of adding two input values.
+	// So, .textContent adds text to DIV
+	sumResult.textContent = input1 + input2;
+
+	// So, we have div, we have class name, we have value in it. But this does not show in browser yet.
+
+	// This adds this new created DIV into DOM to show in browser
+	sum.appendChild(sumResult);
+});
+
+let substract = document.getElementById("subtraction");
+
+subtractForm.addEventListener("submit", function(e) {
+	e.preventDefault();
+	let input3 = parseInt(subtractForm.subtractNum1.value);
+	let input4 = parseInt(subtractForm.subtractNum2.value);
+	let subResult = document.createElement("div");
+	subResult.className = "subResult";
+
+	subResult.textContent = input3 - input4;
+	subtraction.appendChild(subResult);
+});
+
+let multiply = document.getElementById("multiplication");
+
+multiplyForm.addEventListener("submit", function(e) {
+	e.preventDefault();
+	let input5 = parseInt(multiplyForm.num1.value);
+	let input6 = parseInt(multiplyForm.num2.value);
+	let multResult = createElement("div");
+	multResult.className = "multResult";
+
+	multResult.textContent = input5 * input6;
+	multiply.appendChild(multResult);
+});
