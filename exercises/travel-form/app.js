5509fe1dac354caa900a434f998483e0efb5ce11
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2019-11-23 12:03:50 -0700

    Modified travel-form assighment

--- exercises/travel-form/app.js
+++ exercises/travel-form/app.js
@@ -6,17 +6,26 @@ travelForm.addEventListener("submit", function(event) {
 	let lastName = travelForm.lastName.value;
 	let age = travelForm.age.value;
 	let gender = travelForm.gender.value;
-	let kosher = travelForm.diet.value;
-	// let halal = travelForm.diet.value;
-	// let lactoseFree = travelForm.diet.value;
+	let kosherValue = "";
+	// travelForm.kosher.checked && (kosherValue = travelForm.kosher.value);
+	if (travelForm.kosher.checked) {
+		kosherValue = travelForm.kosher.value;
+	}
+
+	let halalValue = "";
+	if (travelForm.halal.checked) {
+		halalValue = travelForm.halal.value;
+	}
+
+	let lactoseFreeValue = "";
+	if (travelForm.lactoseFree.checked)
+		lactoseFreeValue = travelForm.lactoseFree.value;
 
 	alert(
-		`
-        First name: ${firstName} \n
-        Last name:  ${lastName} \n
-        Age: ${age} \n
-        Gender: ${gender} \n
-        Dietery Restrictions: ${kosher} 
-        `
+		`First name: ${firstName} \n
+         Last name:  ${lastName} \n
+         Age: ${age} \n
+         Gender: ${gender} \n
+         Dietery Restrictions: ${kosherValue} ${halalValue} ${lactoseFreeValue}`
 	);
 });
