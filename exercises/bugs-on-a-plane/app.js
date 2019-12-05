67a44f47d2e36924ed8fd963b74fabc5b82c6a12
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2019-12-05 10:27:05 -0700

    Made some changes to app.js

--- exercises/bugs-on-a-plane/app.js
+++ exercises/bugs-on-a-plane/app.js
@@ -6,17 +6,17 @@ submit.addEventListener("click", formAlert);
 function formAlert() {
 	var firstName = form["first-name"].value;
 	var lastName = form["last-name"].value;
-	var age = form["age"].value;
-	var gender = form["gender"].value;
+	var age = form.age.value;
+	var gender = form.gender.value;
 	var location = form["travel-location"].value;
 	var diet = [];
-	if (form["vegan"].checked) {
+	if (form.vegan.checked) {
 		diet.pop(document.getElementById("vegan").value);
 	}
-	if (form["gluten"].checked) {
+	if (form.gluten.checked) {
 		diet.push(document.getElementById("gluten").value);
 	}
-	if (form["paleo"].checked) {
+	if (form.paleo.checked) {
 		diet.push(document.getElementById("paleo").value);
 	}
 
