6bcde97a42d91d595cfbdb0161823fb00d6187d6
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2019-12-22 19:54:28 -0700

    completed axios-practice

---
+++ exercises/axios-practice/app.js
@@ -0,0 +1,38 @@
+
+// const addForm = document.todoform 
+
+// addForm.addEventListener("submit", function (event){
+//     event.preventDefault()
+   
+//     const newTodo = {
+//         title: addForm.title.value,
+//         description: addForm.description.value,
+//         imgUrl: addForm.imgUrl.value    
+//     }
+
+//     axios.post("https://api.vschool.io/sultan/todo", newTodo)
+//     .then(response => console.log(response.data))
+//     .catch(error => console.log(error))
+    
+// })
+
+
+// axios.delete("https://api.vschool.io/sultan/todo/5dffd12d70a96953901db96f")
+// .then(response => console.log(response.data))
+// .catch(error => console.log(error)) 
+
+//  const updateInfo = {
+//      description: "changed it"
+//  }
+
+
+// axios.put("https://api.vschool.io/sultan/todo/5dfba6bafc595641db7f7bff",updateInfo) 
+
+//     .then(response => console.log(response.data))
+//     .catch(error => console.log(error))
+
+
+
+// axios.get("https://api.vschool.io/sultan/todo")
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
