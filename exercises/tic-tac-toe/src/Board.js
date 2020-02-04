ae6575ad3d4cd6950e76791d55c4c2616ec07da5
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2020-02-03 17:21:38 -0700

    Started react exercise Tic tac Toe

---
+++ exercises/tic-tac-toe/src/Board.js
@@ -0,0 +1,86 @@
+import React from "react"
+import Square from "./Square"
+
+
+class Board extends React.Component {
+    constructor(props) {
+        super(props) 
+        this.state = {
+            squares: Array(9).fill(null), 
+            xIsNext: true, 
+        }
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice(); 
+        if (calculateWinner(squares) || squares[i]) {
+            return
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O"
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext, 
+        })
+    }
+
+    renderSqaure(i) {
+        return <Square 
+        value={this.state.squares[i]} 
+        onClick={()=> this.handleClick(i)}
+        />
+    }
+
+    render() {
+        const winner = calculateWinner(this.state.squares)
+        let status
+        if(winner) {
+            status = "Winner: " + winner 
+        } else {
+            status = "Next player: " + (this.state.xIsNext ? "X" : "O")
+        }
+        return (
+            <div className="container"> 
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSqaure(0)}
+                    {this.renderSqaure(1)}
+                    {this.renderSqaure(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSqaure(3)}
+                    {this.renderSqaure(4)}
+                    {this.renderSqaure(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSqaure(6)}
+                    {this.renderSqaure(7)}
+                    {this.renderSqaure(8)}
+                </div>
+            </div>
+        )
+    }
+}
+
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+export default Board
+
