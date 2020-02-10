0651f862dacbd7686887a33200c2ffa356de6c81
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2020-02-10 14:06:11 -0700

    Completed React random color component

---
+++ exercises/react-random-color-component/src/App.js
@@ -0,0 +1,33 @@
+import React from "react" 
+import axios from "axios"
+
+class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            colors: []
+        }
+    }
+
+    componentDidMount() {
+      axios.get("http://www.colr.org/json/colors/random/7")
+      .then((response) => {
+          this.setState({colors: response.data.colors})
+      })
+   }
+
+    render() {
+        const mappedColors = this.state.colors.map((color, i) => {
+            return <h1 key={i} style={{height: 200, width: 200, backgroundColor: `#${color.hex}`}}>{color.hex}</h1>
+        })
+        return (
+                <div> 
+                    {mappedColors}
+                </div>
+            )
+    }
+    
+    
+}
+
+export default App
