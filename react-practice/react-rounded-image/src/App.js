94e8e2b7df3c45b968bc2132cae9f3d7f5807350
Beksultan Zhanybekov <zhanybekov.b@gmail.com>
2020-02-16 11:15:35 -0700

    Completed React rounded image exercise

---
+++ react-practice/react-rounded-image/src/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import RoundedImg from "./RoundedImg"
+
+const App = () => {
+    return (
+        <div> 
+            <RoundedImg src="https://picsum.photos/id/237/300/300" borderRadius="50%" /> 
+        </div>
+    )
+
+}
+
+export default App 
